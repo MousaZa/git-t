d47dea3035cf396e530d09c965e8db5f2ad0917a
MousaZa <mous.zeydan@gmail.com>
2025-05-11 21:38:51 +0300

    Divide

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import "fmt"
 func main() {
 	fmt.Printf("1 + 2 = %d\n", add(1, 2))
 	fmt.Printf("5 - 3 = %d\n", subtract(5, 3))
+	fmt.Printf("4 * 2 = %d\n", multiply(4, 2))
+	fmt.Printf("8 / 2 = %d\n", divide(8, 2))
 }
 
 func add(a, b int) int {
@@ -17,4 +19,11 @@ func subtract(a, b int) int {
 
 func multiply(a, b int) int {
 	return a * b
+}
+
+func divide(a, b int) int {
+	if b == 0 {
+		panic("division by zero")
+	}
+	return a / b
 }
