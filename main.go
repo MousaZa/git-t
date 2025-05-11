89110807bdf0deb168d17b388a18b8f1b8d02525
MousaZa <mous.zeydan@gmail.com>
2025-05-11 21:23:18 +0300

    Sub

--- main.go
+++ main.go
@@ -4,8 +4,13 @@ import "fmt"
 
 func main() {
 	fmt.Printf("1 + 2 = %d\n", add(1, 2))
+	fmt.Printf("5 - 3 = %d\n", subtract(5, 3))
 }
 
 func add(a, b int) int {
 	return a + b
+}
+
+func subtract(a, b int) int {
+	return a - b
 }
